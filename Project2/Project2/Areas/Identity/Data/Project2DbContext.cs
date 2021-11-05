9989437db3564ec160fb9d1101cea222de29f827
shene <sheneboshoff6@gmail.com>
2021-11-05 14:09:40 +0200

    Add project files.

---
+++ Project2/Project2/Areas/Identity/Data/Project2DbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project2.Areas.Identity.Data;
+
+namespace Project2.Data
+{
+    public class Project2DbContext : IdentityDbContext<ApplicationUser>
+    {
+        public Project2DbContext(DbContextOptions<Project2DbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Project2/Project2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project2.Areas.Identity.Data;
+using Project2.Data;
+
+[assembly: HostingStartup(typeof(Project2.Areas.Identity.IdentityHostingStartup))]
+namespace Project2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Project2DbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Project2DbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => 
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<Project2DbContext>();
+            });
+        }
+    }
+}
